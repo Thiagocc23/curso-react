d55990ebdc83cc99f58ccced41a031a28a7738af
Thiagocc23 <148499649+Thiagocc23@users.noreply.github.com>
2024-06-17 21:01:12 -0300

    Update Tipodealojamiento.js

--- src/client/src/formularios/Tipodealojamiento.js
+++ src/client/src/formularios/Tipodealojamiento.js
@@ -1,116 +1,176 @@
-import React, { useState } from 'react';
-import '../styles/Formularios.css';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Tipodealojamiento = () => {
-  const [formData, setFormData] = useState({
-    tipoAlojamiento: '',
-  });
+const TipoAlojamiento = () => {
+  const [descripcion, setDescripcion] = useState('');
+  const [tipos, setTipos] = useState([]);
 
-  const [editIndex, setEditIndex] = useState(null);
-  const [data, setData] = useState([]);
-  const [idCounter, setIdCounter] = useState(1); 
+  useEffect(() => {
+    getAlojamientos();
+  }, []);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (!formData.tipoAlojamiento) {
-      alert('Por favor, complete el campo del tipo de alojamiento.');
+  const enviar = async (evento) => {
+    evento.preventDefault();
+    if (!descripcion.trim()) {
+      alert('Por favor ingresa una descripción');
       return;
     }
+    const json = {
+      Descripcion: descripcion,
+    };
 
-    if (editIndex !== null) {
-      const updatedData = data.map((item, index) => (index === editIndex ? { ...item, ...formData } : item));
-      setData(updatedData);
-      setEditIndex(null);
-    } else {
-      setData([...data, { ...formData, id: idCounter }]);
-      setIdCounter(idCounter + 1); 
+    try {
+      const respuesta = await fetch(
+        'http://localhost:3001/tiposAlojamiento/createTipoAlojamiento',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(json),
+        }
+      );
+      if (respuesta.ok) {
+        alert('Se creó correctamente el Tipo Alojamiento');
+        setDescripcion('');
+        getAlojamientos();
+      } else {
+        alert('ERROR!! No se creó el Tipo Alojamiento');
+      }
+    } catch (error) {
+      console.error('Failed to create:', error);
     }
-
-    setFormData({
-      tipoAlojamiento: '',
-    });
   };
 
-  const handleEdit = (index) => {
-    setFormData(data[index]);
-    setEditIndex(index);
+  const getAlojamientos = () => {
+    fetch('http://localhost:3001/tiposAlojamiento/getTiposAlojamiento')
+      .then((res) => res.json())
+      .then((res) => {
+        if (Array.isArray(res)) {
+          setTipos(res);
+        } else {
+          console.error('ERROR: Array esperada pero no obtenida', res);
+        }
+      })
+      .catch((error) => console.error('Error recuperación de datos:', error));
   };
 
-  const handleDelete = (index) => {
-    const updatedData = data.filter((_, i) => i !== index);
-    setData(updatedData);
+  const Borrar = async (id) => {
+    try {
+      const respuesta = await fetch(
+        `http://localhost:3001/tiposAlojamiento/deleteTipoAlojamiento/${id}`,
+        {
+          method: 'DELETE',
+        }
+      );
+      if (respuesta.ok) {
+        alert('Se eliminó correctamente el Tipo Alojamiento');
+        console.log('BORRADO EXITOSO:', respuesta);
+        getAlojamientos();
+      } else {
+        console.error('ERROR al borrar:', respuesta);
+        alert('ERROR!! No se eliminó el Tipo Alojamiento');
+      }
+    } catch (err) {
+      console.error('Error al eliminar datos:', err);
+      alert('ERROR!! No se eliminó el Tipo Alojamiento');
+    }
+  };
+
+  const Editar = async (id) => {
+    if (!descripcion.trim()) {
+      alert('Por favor ingresa una descripción');
+      return;
+    }
+    const json = {
+      Descripcion: descripcion,
+    };
+    try {
+      const respuesta = await fetch(
+        `http://localhost:3001/tiposAlojamiento/putTipoAlojamiento/${id}`,
+        {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(json),
+        }
+      );
+      if (respuesta.ok) {
+        alert('Se modificó el elemento');
+        console.log('Corrección Exitosa:', respuesta);
+        setDescripcion('');
+        getAlojamientos();
+      } else {
+        console.error('ERROR al modificar:', respuesta);
+        alert('ERROR!! No se modificó el Tipo Alojamiento');
+      }
+    } catch (err) {
+      console.error('ERROR al modificar los datos:', err);
+      alert('ERROR!! No se modificó el Tipo Alojamiento');
+    }
   };
 
   return (
-    <>
-      <section className="formulario">
-        <h2>Registra tu Tipo de Alojamiento</h2>
-        <div className="cuadro">
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label className="label" htmlFor="tipoAlojamiento">Tipo de Alojamiento</label>
-              <select
-                className="casilla"
-                id="tipoAlojamiento"
-                name="tipoAlojamiento"
-                value={formData.tipoAlojamiento}
-                onChange={handleChange}
-                required
-              >
-                <option value="">Seleccione un Tipo de Alojamiento</option>
-                <option value="Casa">Casa</option>
-                <option value="Departamento">Departamento</option>
-                <option value="Habitacion">Habitación</option>
-                <option value="Hotel">Hotel</option>
-                <option value="Cabaña">Cabaña</option>
-              </select>
-            </div>
-            <div className="boton-container">
-              <button type="submit">{editIndex !== null ? 'Guardar cambios' : 'Registrar'}</button>
-            </div>
-          </form>
-        </div>
-      </section>
-
-      <section className="tabla">
-        <div className="contenido-centrado">
-          <div className="tabla-container">
-            <h2>Tipos de Alojamiento Registrados</h2>
-            <table id="datosTabla">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Tipo de alojamiento</th>
-                  <th>Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((item, index) => (
-                  <tr key={item.id}>
-                    <td>{item.id}</td>
-                    <td>{item.tipoAlojamiento}</td>
-                    <td>
-                      <button className="edit-btn" onClick={() => handleEdit(index)}>Editar</button>
-                      <button className="delete-btn" onClick={() => handleDelete(index)}>Eliminar</button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+    <div className="p-4 container">
+      <div className="formularioAlojamiento col p-3">
+        <h1>Alta de Tipo Alojamiento</h1>
+        <form onSubmit={enviar}>
+          <div>
+            <label className="p-3" htmlFor="descripcion">
+              Descripción:
+            </label>
+            <input
+              className="col-sm-5 col-form-label mt-3 rounded-4 form-control"
+              name="descripcion"
+              id="descripcion"
+              value={descripcion}
+              type="text"
+              placeholder="Descripción Tipo Alojamiento"
+              aria-label=".form-control-lg example"
+              onChange={(evento) => setDescripcion(evento.target.value)}
+            />
           </div>
-        </div>
-      </section>
-    </>
+          <button className="btn btn-success mt-3 col-sm-3" type="submit">
+            Enviar
+          </button>
+        </form>
+        <table className="table col">
+          <thead>
+            <tr>
+              <th scope="col" className="table-primary">
+                Tipo Alojamiento
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.isArray(tipos) ? (
+              tipos.map((tipo) => (
+                <tr key={tipo.idTipoAlojamiento}>
+                  <td>{tipo.Descripcion}</td>
+                  <div className="mb-2 bg-transparent d-flex align-items-center">
+                    <button
+                      onClick={() => Borrar(tipo.idTipoAlojamiento)}
+                      className="btn btn-danger"
+                    >
+                      Eliminar
+                    </button>
+                    <button
+                      onClick={() => Editar(tipo.idTipoAlojamiento)}
+                      className="btn btn-warning"
+                    >
+                      Editar
+                    </button>
+                  </div>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="3"></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
-export default Tipodealojamiento;
+export default TipoAlojamiento;
+
